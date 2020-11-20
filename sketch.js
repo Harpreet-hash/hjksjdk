1e466700bdb328ecd2f2fe19dbac8469aaaab99d
Harpreet Kaur <hrprtkr1998@gmail.com>
2020-11-20 13:14:10 +0530

    Platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-    platform = new Ground(200,height,500,400)
+    platform = new Ground(200,height,500,400);
 
 
     box1 = new Box(700,320,70,70);
